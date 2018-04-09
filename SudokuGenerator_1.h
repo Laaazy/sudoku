2734a82c3cddb4d5c3f8142265a6ff9186b626d5
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-04-10 00:29:18 +0800

    Update SudokuGenerator_1.h

--- SudokuGenerator_1.h
+++ SudokuGenerator_1.h
@@ -1,9 +1,11 @@
 #pragma once
 #include<vector>
+#include "DLXGenerator_1.h"
+#include "SudokuSolver_1.h"
 
 using namespace std;
 
-class SudokuGeenerator
+class SudokuGenerator_1
 {
 public:
 	vector<vector<int>> generateSudokus(int sudokuCount);
