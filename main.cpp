4b03ca8800d2336d2d7283f4f0b8b629ad702555
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-04-19 17:27:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"SudokuLoader_1.h"
+#include"SudokuGenerator_1.h"
 #define SudokuMax 1000000
 
 using namespace std;
@@ -7,6 +8,40 @@ using namespace std;
 void reportError() 
 {
 	cout << "Program argument error" << endl;
+}
+
+void solvePuzzle(fstream& puzzleFile) {
+	
+	SudokuLoader_1 loader = SudokuLoader_1();
+	vector<vector<int>> sudokuSet = loader.loadFromFile(puzzleFile);
+	int sudokuCount = sudokuSet.size();
+
+	
+	vector<vector<int>> answers;
+	answers.resize(sudokuCount);
+
+	for (unsigned int j = 0; j < sudokuSet.size(); ++j)
+	{
+		SudokuSolver_1 solver = SudokuSolver_1();
+		DLXNode_1* listHead = new DLXNode_1();
+		vector<int> answer;
+
+		if (solver.solveSudoku(listHead, sudokuSet[j], answer)) 
+			answers[j] = answer;
+		else 
+			cout << "No solution for sudoku No." << j + 1 << endl;
+	}
+
+	
+	fstream solutionFile;
+	solutionFile.open("C:\\Visual_Studio_C_Programs\\Sudoku\\sudoku\\Debug\\sudoku.txt", ios::out);
+	if (answers.size() == 0) {
+		cout << "No solutions for all sudokus" << endl;
+	}
+	else {
+		loader.writeToFile(answers, solutionFile);
+	}
+	solutionFile.close();
 }
 
 void createSudoku(fstream& sudokuFile, int sudokuCount)
@@ -31,13 +66,16 @@ int main(int argc, char *argv[])//命令行启动程序参数个数，命令行�
 	if (strcmp(argv[1], "-c") == 0 && atoi(argv[2]) > 0 && atoi(argv[2]) <= SudokuMax)//生成数独终局命令，且生成数量合法 
 	{
 		fstream sudokuFile;
-		sudokuFile.open("sudoku.txt", ios::out);
+		sudokuFile.open("C:\\Visual_Studio_C_Programs\\Sudoku\\sudoku\\Debug\\sudoku.txt", ios::out);
 		createSudoku(sudokuFile, atoi(argv[2]));
 		sudokuFile.close();
 	}
 	else if (strcmp(argv[1], "-s") == 0)//求解数独命令
 	{
-		;
+		fstream puzzleFile;
+		puzzleFile.open(argv[2], ios::in);
+		solvePuzzle(puzzleFile);
+		puzzleFile.close();
 	}
 	else
 		reportError();
