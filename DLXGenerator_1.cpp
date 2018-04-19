79a422ae93ef7dadf4c5ab41327269aa3710c0f1
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-04-19 17:31:17 +0800

    Update DLXGenerator_1.cpp

--- DLXGenerator_1.cpp
+++ DLXGenerator_1.cpp
@@ -9,25 +9,27 @@ vector<ColumnHead_1 *> DLXGenerator_1::createColumnHeads(DLXNode_1 *listHead, in
 
 	for (int i = 0; i < length; ++i) 
 	{
-		ColumnHead_1 *columnHead = new ColumnHead_1(i);
+		ColumnHead_1 *columnHead = new ColumnHead_1(i);//i是构造函数参数，赋予ColumnHead列号
 		lastNode->appendRightNode(columnHead);
-		columnHeads[i] = columnHead;
-		lastNode = columnHead;
+		columnHeads[i] = columnHead;//将当前列头保存至列头数组
+		lastNode = columnHead;//更新lastNode
 	}
 
-	lastNode->appendRightNode(listHead);
+	lastNode->appendRightNode(listHead);//构成循环链表
 	return columnHeads;
 }
 
 //加入一行元素
-void DLXGenerator_1::appendLine(vector<ColumnHead_1*> columnHeads, vector<int> elementSubscripts, int rowIndex) {
+void DLXGenerator_1::appendLine(vector<ColumnHead_1*> columnHeads, vector<int> elementSubscripts, int rowIndex) 
+{
 	CommonNode_1 *lastHorizontalNode = NULL;
 	CommonNode_1 *firstHorizontalNode = NULL;
 
-	for (unsigned int i = 0; i < elementSubscripts.size(); ++i) {
+	for (int i = 0; i < elementSubscripts.size(); ++i) 
+	{
 		int subscript = elementSubscripts[i];
-		ColumnHead_1 *columnHead = columnHeads[subscript];
-		CommonNode_1 *currentNode = new CommonNode_1(rowIndex, columnHead);
+		ColumnHead_1 *columnHead = columnHeads[subscript];//此处有不确定改动
+		CommonNode_1 *currentNode = new CommonNode_1(rowIndex, columnHead);//构造新内部节点并初始化行号列号
 		DLXNode_1 *lastVerticalNode = columnHead->upNode;
 
 		//链接垂直节点
@@ -35,11 +37,13 @@ void DLXGenerator_1::appendLine(vector<ColumnHead_1*> columnHeads, vector<int> e
 		currentNode->appendDownNode(columnHead);
 
 		//链接水平节点
-		if (i == 0) {
+		if (i == 0)
+		{
 			firstHorizontalNode = currentNode;
 			lastHorizontalNode = currentNode;
 		}
-		else {
+		else 
+		{
 			lastHorizontalNode->appendRightNode(currentNode);
 			lastHorizontalNode = currentNode;
 		}
