0576219029afea1772c29551a2e64581590bf048
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-04-19 17:28:25 +0800

    Update DLXGenerator_1.h

--- DLXGenerator_1.h
+++ DLXGenerator_1.h
@@ -6,7 +6,8 @@ using namespace std;
 
 class DLXGenerator_1
 {
+public:
 	vector<ColumnHead_1*> createColumnHeads(DLXNode_1* listHead, int length);
 
-	void appendLine(vector<ColumnHead_1*> columnHeads, vector<int> elementSubscripts, int rowIndex);
+	void appendLine(vector<ColumnHead_1*> columnHeads, vector<int> elementSubscripts, int rowIndex);//附加一行元素
 };
